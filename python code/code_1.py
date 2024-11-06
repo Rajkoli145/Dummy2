6afbd9f03d077e1533e5df3c0253aeb9a87d80d4
Rajkoli145 <2024.rajk@isu.ac.in>
2024-11-06 11:57:36 +0530

    Frist commit

---
+++ python code/code_1.py
@@ -0,0 +1,4 @@
+import ModuleEX1
+print(ModuleEX1.add(5,10))
+print(ModuleEX1.prod(10,20))
+print(ModuleEX1.x)
---
+++ python code/mathsfunction2.py
@@ -0,0 +1,12 @@
+import math
+print(math.fmod(20 ,4))
+print(math.fmod(20,3))
+print(math.fmod(-10,3))
+
+print(math.fsum([1,2,3,4,5]))
+print(math.fsum((100,400,500)))
+print(math.fsum((100,400,500)))
+print(math.fsum(range(0,5)))
+print(math.fsum({1:"one",2:"two",3:3,4:4}))
+print(math.fsum( {7,2,3,4}))
+ 
