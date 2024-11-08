d9fa3fe652f78358510ce47626e318acceab533f
Rajkoli145 <2024.rajk@isu.ac.in>
2024-11-08 10:35:59 +0530

    Intial commit: Added Myfile.txt

--- python code/ModuleEX1.py
+++
@@ -1,7 +0,0 @@
-x=12
-def add(a,b):
-    sum=a+b
-    return sum
-def prod(a,b):
-    p=a*b
-    return p
--- python code/code_1.py
+++
@@ -1,4 +0,0 @@
-import ModuleEX1
-print(ModuleEX1.add(5,10))
-print(ModuleEX1.prod(10,20))
-print(ModuleEX1.x)
